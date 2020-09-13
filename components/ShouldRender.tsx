7314db41af6893ca4bff08b905ca6717d6023cab
Ayodele Olalekan <longyarnz@gmail.com>
2020-09-14 00:22:10 +0100

    chore(master): edit prop types on ShouldRender

--- components/ShouldRender.tsx
+++ components/ShouldRender.tsx
@@ -1,17 +1,15 @@
 import React, { ReactElement } from 'react'
 
 type ShouldRenderProps = {
-  if: boolean
-  children: ReactElement
+  if: any
+  children: any
 }
 
 /**
  * @name ShouldRender
- * @param {boolean} props.if if true or truthy, the children element is rendered
- * @param {ReactElement} props.children Children element/component to be rendered
+ * @param {any} if resolves to true or truthy value and determines whether the children element is rendered or not
+ * @param {ReactElement} children Children element/component to be rendered
  */
 export const ShouldRender: React.FC<ShouldRenderProps> = (props) => {
-  return props.if ? (
-    props.children
-  ) : null;
+  return props.if ? props.children : null
 }
